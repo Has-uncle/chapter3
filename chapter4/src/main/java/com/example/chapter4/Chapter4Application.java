06f61f8b4ce6bc5817b8e690edfa0b4938ccf560
Has-uncle <2179959781@qq.com>
2019-03-21 08:01:02 +0800

    2019/03/21 08：00

---
+++ chapter4/src/main/java/com/example/chapter4/Chapter4Application.java
@@ -0,0 +1,16 @@
+package com.example.chapter4;
+
+import com.example.chapter4.service.HelloService;
+import com.example.chapter4.service.impl.HelloServiceImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter4Application.class, args);
+
+    }
+
+}
